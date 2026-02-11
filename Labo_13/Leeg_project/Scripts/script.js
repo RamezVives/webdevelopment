6bc6935f0cf5b30ce2a6919e9995ee490ac10e4d
RamezVives <r1101973@student.vives.be>
2026-02-11 06:43:53 +0100

    update

---
+++ Labo_13/Leeg_project/Scripts/script.js
@@ -0,0 +1,7 @@
+const setup = () => {
+    // deze code wordt pas uitgevoerd
+    // als de pagina volledig is ingelade
+    console.log("Pagina is geladen");
+}
+
+window.addEventListener("load", setup);
